002ae5b3724a325d669798feb4b0f2e8b525cd8c
Ryo Masuda <mrra4611@gmail.com>
2018-02-22 15:46:32 +0900

    Add expensify styles

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -4,16 +4,25 @@ import ExpenseItemList from './ExpenseItemList';
 import selectExpenses from '../selectors/expenses';
 
 export const ExpenseList = (props) => (
-  <div>
+  <div className="content-container">
+    <div className="list-header">
+      <div className="show-for-mobile">Expenses</div>
+      <div className="show-for-desktop">Expense</div>
+      <div className="show-for-desktop">Amount</div>
+    </div>
+    <div className="list-body">
     {
       props.expenses.length === 0 ? (
-        <p>No Expense</p>
-      ) : (
-        props.expenses.map((expense) => {
-          return <ExpenseItemList key={expense.id} { ...expense } />;
-        })
-      )
-    }
+        <div className="list-item list-item__message">
+          <span>No Expenses</span>
+        </div> 
+          ) : (
+              props.expenses.map((expense) => {
+                return <ExpenseItemList key={expense.id} { ...expense } />;
+              })
+            )
+          }
+    </div>
   </div>
 )
 
